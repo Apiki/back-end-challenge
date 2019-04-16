cc9f9d08efd9a3ad82bde047430c5cfa093eef27
Joao Augusto <augusto_casotti@hotmail.com>
2019-04-16 14:31:20 -0300

    Feito o teste con sucesso

--- calc.php
+++ calc.php
@@ -1,29 +1,54 @@
 <?php
-include("index.php");
+class moeda
+{
+    public function euro($euro)
+    {
+        return $euro = 4.40;
+    }
+    public function dolar($dolar)
+    {
+        return $dolar = 3.90;
+    }
+    public function dolae($eurodola)
+    {
+        return $eurodola=1.13;
+    }
+    public function realeuro($realeuro)
+    {
+        return $realeuro=0.23;
+    }
+    public function realdola($realdola)
+    {
+      return $realdola=0.26;  
+    }
+}
+
+include('index.php');
+$mod = new moeda();
 
 if (!empty($_POST['valor1'])) {
-    $val =floatval($_POST['valor1']);
+    $val = floatval($_POST['valor1']);
     $tipo1 = $_POST['de'];
     $tipo2 = $_POST['para'];
+
     if ($tipo1 == 'Euro' && $tipo2 == 'Real') {
-        $total = $val * 4.39;
-        echo "O valor Convertido é: R$" .money_format('%#2n', $total);
+        $total = $val * $mod->euro($euro);
+        echo "O valor Convertido é: R$" . money_format('%#2n', $total);
     }
-    if ($tipo1 == 'Dola' && $tipo2=='Real') {
-        $total= $val * 3.88;
-        echo "O valor convertido é: R$".money_format('%2n', $total);
+    if ($tipo1 == 'Dola' && $tipo2 == 'Real') {
+        $total = $val * $mod->dolar($dolar);
+        echo "O valor convertido é: R$" . money_format('%2n', $total);
     }
-    if ($tipo1 == 'Euro' && $tipo2=='Dola') {
-        $total= $val * 1.13;
-        echo "O valor convertido é: USD".money_format('%2n', $total);
+    if ($tipo1 == 'Euro' && $tipo2 == 'Dola') {
+        $total = $val * $mod->dolae($eurodola);
+        echo "O valor convertido é: USD" . money_format('%2n', $total);
     }
-    if ($tipo1 == 'Real' && $tipo2=='Euro') {
-        $total= $val / 0.23;
-        echo "O valor convertido é: EUD".money_format('%2n', $total);
+    if ($tipo1 == 'Real' && $tipo2 == 'Euro') {
+        $total = $val * $mod->realeuro($realeuro);
+        echo "O valor convertido é: EUD" . money_format('%2n', $total);
     }
-    if ($tipo1 == 'Real' && $tipo2=='Dola') {
-        $total= $val / 0.26;
-        echo "O valor convertido é: EUD".money_format('%2n', $total);
+    if ($tipo1 == 'Real' && $tipo2 == 'Dola') {
+        $total = $val * $mod->realdola($realdola);
+        echo "O valor convertido é: EUD" . money_format('%2n', $total);
     }
-    
 }
