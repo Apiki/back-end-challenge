2ce2b36228fbd06ab919fa6ba35b7b8d0ccb50eb
Daniel Ferraz Ramos <daniel@art2web.com.br>
2020-07-09 15:39:19 -0300

    Apki API Exchange pure PHP
    changed to pure PHP

--- index.php
+++ index.php
@@ -14,4 +14,28 @@
  */
 declare(strict_types=1);
 
-require __DIR__ . '/vendor/autoload.php';
+//require __DIR__ . '/vendor/autoload.php';
+
+include('class/Route.php');
+
+Route::add('/',function(){
+    echo '<h1>Apki API Exchange</h1>';
+    echo '<h3>usage: http://localhost:8000/exchange/{amount}/{from}/{to}/{rate}</h3>';
+});
+
+Route::add('/exchange/([0-9.-]+)/([A-Z]+)/([A-Z]+)/([0-9.-]+)', function($amount, $from, $to, $rate) {
+    $moedas = [
+                'BRL' => 'R$',
+                'USD' => '$',
+                'EUR' => '€'
+            ];
+    $result = $amount * $rate;
+    $json = [
+            'valorConvertido' => $result,
+            'simboloMoeda'    => $moedas[$to]
+        ];
+    $data = json_encode($json, JSON_UNESCAPED_UNICODE);
+    echo $data;
+}, 'get');
+
+Route::run('/');
