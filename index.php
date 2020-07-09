b5ca06cbcee4619cc34c7f4afd6b6c196428afcd
Daniel Ferraz Ramos <daniel@art2web.com.br>
2020-07-09 16:45:27 -0300

    include json response function
    commit

--- index.php
+++ index.php
@@ -74,3 +74,29 @@ Route::add('/exchange/([0-9.]+)/([A-Z]+)/([A-Z]+)/([0-9.]+)', function($amount, 
 }, 'get');
 
 Route::run('/');
+
+
+function json_response($code = 200, $message = null)
+{
+    // clear the old headers
+    header_remove();
+    // set the actual code
+    http_response_code($code);
+    // set the header to make sure cache is forced
+    header("Cache-Control: no-transform,public,max-age=300,s-maxage=900");
+    // treat this as json
+    header('Content-Type: application/json');
+    $status = array(
+        200 => '200 OK',
+        400 => '400 Bad Request',
+        422 => 'Unprocessable Entity',
+        500 => '500 Internal Server Error'
+        );
+    // ok, validation error, or failure
+    header('Status: '.$status[$code]);
+    // return the encoded json
+    return json_encode(array(
+        'status' => $code < 300, // success or not?
+        'message' => $message
+        ));
+}
