4cb37f4f54bf3a22d392810ab41f344ef7a0c165
Adler de Oliveira <50225198+adlerdeoliveira@users.noreply.github.com>
2020-07-19 15:35:33 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
 <?php
-	
+/**
+ * Back-end Challenge.
+ *
+ * PHP version 7.2
+ *
+ * Este será o arquivo chamado na execução dos testes automátizados.
+ *
+ * @category Challenge
+ * @package  Back-end
+ * @author   Adler Oliveira <adler.deoliveira@gmail.com>
+ * @license  http://opensource.org/licenses/MIT MIT
+ * @link     https://github.com/apiki/back-end-challenge
+ */
+ 	
 	header('Content-Type: application/json; charset=utf-8');
 
 	require_once 'API/APIKI/Apiki.php';
