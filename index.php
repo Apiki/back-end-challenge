7ca1fb8767ef41f46960df73e47ab622e1cf5e60
Adler de Oliveira <50225198+adlerdeoliveira@users.noreply.github.com>
2020-07-19 19:15:00 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -12,14 +12,97 @@
  * @license  http://opensource.org/licenses/MIT MIT
  * @link     https://github.com/apiki/back-end-challenge
  */
-	
-	declare(strict_types=1, encoding='UTF-8');
-	
-
+	declare(strict_types = 1);
 	require __DIR__ . '/vendor/autoload.php';
-	require 'API/APIKI/Apiki.php';
-
-	if (isset($_REQUEST)) {		echo REST_API_APIKI::abrir($_REQUEST);		}
 
 
+require_once 'API/APIKI/Exchange.php';
+
+class REST_API_APIKI
+{
+
+
+	public function abrir($chamar) 
+	{
+
+
+		if (isset($_REQUEST)) {
+
+
+			
+			if (substr_count($chamar['url'], '/') >0) {
+
+
+			    $dados = explode('/', $chamar['url']);
+			} else {
+
+					return json_encode(array(
+	                    'status'      =>  400,
+	                    'message'   =>  'Bad Request'
+	                ), 400); 				
+			}
+
+
+
+
+		    if (count($dados) !== 4 || $dados[0] == "") { 
+
+		    	//http_response_code(400);
+		    	//return json_encode(http_response_code(400));
+				return json_encode(array(
+                    'status'      =>  400,
+                    'message'   =>  'Bad Request'
+                ), 400); 		    	
+               	//return;
+		    }
+
+
+		    $amount 	= $dados[0];	//Parâmetro - Quantodade de moedas
+		    $from 		= $dados[1];	//Parâmetro - Moeda de atual
+		    $to 		= $dados[2];	//Parâmetro - Moeda de desejada
+		    $rate 		= $dados[3];	//Parâmetro - Taxa da moeda desejada
+
+		    $classe 	= 'Exchange';	//Classe - Contém a função responsável pela conversão da moeda
+		    $metodo 	= 'convertCoin';//Função - Convenverte a moeda
+		    $parametros = array();
+		    $parametros = $dados;
+
+		    //valido os parâmetros passados pelo usuário na url		    
+			$amount_tmp	 = str_replace('.', '',  $amount);
+			$amount_tmp	 = str_replace(',', '',  $amount_tmp);
+
+			$rate_tmp	 = str_replace('.', '',  $rate);
+			$rate_tmp	 = str_replace(',', '',  $rate_tmp);
+
+		    if ($rate == '' || $amount =='' || $from =='' || $to =='' || !is_numeric($amount_tmp) || !is_numeric($rate_tmp)) { 
+
+		    	http_response_code(400);
+               	return;
+		    }
+		    /***********final-validação*************/
+
+		    if (class_exists($classe)) {
+
+		    	if (method_exists($classe, $metodo)) {
+		    		$retorno = call_user_func_array(array(new $classe, $metodo), array($parametros));
+
+		    		return json_encode(array($retorno));
+		    		
+		    	}	
+
+		    } else {
+
+		    	http_response_code(400);
+               	return;
+
+		    }
+
+
+
+
+		}		
+	}
+}
+
+echo REST_API_APIKI::abrir($_REQUEST);
 ?>
