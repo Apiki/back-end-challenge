6aa572efa4a92c5300afe9f370e1d4855838850e
Adler de Oliveira <50225198+adlerdeoliveira@users.noreply.github.com>
2020-07-20 01:03:27 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php
 /**
  * Back-end Challenge.
  *
@@ -12,76 +13,9 @@
  * @link     https://github.com/apiki/back-end-challenge
  */
 	declare(strict_types = 1);
-	require __DIR__ . '/vendor/autoload.php';
 
 
-require_once 'API/APIKI/Exchange.php';
-
-class REST_API_APIKI
-{
-
-
-	public function abrir($requisicao) 
-	{
-
-
-		if (isset($_REQUEST)) {
-
-
-			if (substr_count($_SERVER['REQUEST_URI'], '/') >=5) {
-
-			    $dados = explode('/', $requisicao['url']);
-
-			} else {
-
-		    	http_response_code(400);
-               	return;
-			}
-
-
-		    $amount 	= $dados[0];	//Parâmetro - Quantodade de moedas
-		    $from 		= $dados[1];	//Parâmetro - Moeda de atual
-		    $to 		= $dados[2];	//Parâmetro - Moeda de desejada
-		    $rate 		= $dados[3];	//Parâmetro - Taxa da moeda desejada
-
-		    $classe 	= 'Exchange';	//Classe - Contém a função responsável pela conversão da moeda
-		    $metodo 	= 'convertCoin';//Função - Convenverte a moeda
-		    $parametros = array();
-		    $parametros = $dados;
-
-		    //valido os parâmetros passados pelo usuário na url		    
-			$amount_tmp	 = str_replace('.', '',  $amount);
-			$amount_tmp	 = str_replace(',', '',  $amount_tmp);
-
-			$rate_tmp	 = str_replace('.', '',  $rate);
-			$rate_tmp	 = str_replace(',', '',  $rate_tmp);
-
-			$moedas 	= array('BRL', 'USD', 'EUR');
-
-
-		    if (count($dados) !== 4 || $dados[0] == "" || $rate == '' || $amount =='' || $from =='' || $to =='' || !is_numeric($amount_tmp) || !is_numeric($rate_tmp) || !in_array($from, $moedas) || !in_array($from, $moedas)) { 
-
-		    	http_response_code(400);
-               	return;
-		    }
-		    /***********final-validação*************/
-
-		    if (class_exists($classe)) {
-
-		    	if (method_exists($classe, $metodo)) {
-		    		$retorno = call_user_func_array(array(new $classe, $metodo), array($parametros));
-
-		    		return json_encode(array($retorno));
-
-		    	}	
-		    }
-
-
-
-
-		}		
-	}
-}
+	require_once 'API/APIKI/ConvertCoin.php';
 
 	$open = new REST_API_APIKI();
 	echo($open->abrir($_REQUEST));
