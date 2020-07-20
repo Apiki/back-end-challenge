dff6f9f167f758402a8ec44ed30cf4b555185725
Adler de Oliveira <50225198+adlerdeoliveira@users.noreply.github.com>
2020-07-20 01:04:13 -0300

    Add files via upload

---
+++ API/APIKI/ConvertCoin.php
@@ -0,0 +1,82 @@
+<?php
+
+declare(strict_types = 1);
+
+require 'Exchange.php';
+
+class REST_API_APIKI
+{
+ 
+ 
+    public function abrir($chamar) 
+    {
+ 
+ 
+        if (isset($_REQUEST)) {
+ 
+ 
+
+	 		if (substr_count($_SERVER['REQUEST_URI'], '/') >=5) {
+
+
+	            $dados = explode('/', $chamar['url']);
+	 
+	        } else { 
+
+               	http_response_code(400);
+               	return;
+	        }
+ 
+
+            if (count($dados) !== 4) { 
+ 		    	
+               	http_response_code(400);
+               	return;
+               	
+            }
+ 
+            $amount 	= $dados[0];	//Parâmetro - Quantodade de moedas
+            $from 		= $dados[1];	//Parâmetro - Moeda de atual
+            $to 		= $dados[2];	//Parâmetro - Moeda de desejada
+            $rate 		= $dados[3];	//Parâmetro - Taxa da moeda desejada
+ 
+            $classe 	= 'Exchange';	//Classe - Contém a função responsável pela conversão da moeda
+            $metodo 	= 'convertCoin';//Função - Convenverte a moeda
+            
+            //valido os parâmetros passados pelo usuário na url		    
+            $amount_tmp	 = str_replace('.', '',  $amount);
+            $amount_tmp	 = str_replace(',', '',  $amount_tmp);
+            $rate_tmp	 = str_replace('.', '',  $rate);
+            $rate_tmp	 = str_replace(',', '',  $rate_tmp);
+
+            $moedas 	= array('BRL', 'USD', 'EUR');
+ 
+
+		    if (count($dados) !== 4 || $dados[0] == "" || $rate == '' || $amount =='' || $from =='' || $to =='' || !is_numeric($amount_tmp) || !is_numeric($rate_tmp) || !in_array($from, $moedas) || !in_array($from, $moedas)) { 
+
+		    	http_response_code(400);
+               	return;
+            }
+            /***********final-validação*************/
+ 
+            if (class_exists($classe)) {
+ 
+                if (method_exists($classe, $metodo)) {
+                    $retorno = call_user_func_array(array(new $classe, $metodo), array($dados));
+ 
+                    return json_encode($retorno);
+ 		    		
+                }	
+ 
+            } else {
+ 
+		    	http_response_code(400);
+               	return;
+            }
+ 
+ 
+ 
+ 
+        }		
+    }
+ }
