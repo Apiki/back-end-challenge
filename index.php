60f83e3b694bc61038ca6e8e890c4ff56bd71dfd
Adler de Oliveira <50225198+adlerdeoliveira@users.noreply.github.com>
2020-07-21 02:01:43 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -17,25 +17,25 @@
     declare (strict_types=1);
     require 'class/Router.php';
 
-    require __DIR__ . '/vendor/autoload.php';
+    //require __DIR__ . '/vendor/autoload.php';
 
     //Validação de erros de requisição na url
     $parametros = explode('/', $_SERVER['REQUEST_URI']);
     
-    if (count($parametros) <6) { echo retornarErro(400); return null;  }
+    if (count($parametros) <6) { echo retornarErro(); return null;  }
 
-    Route::add('/',function() { echo retornarErro(400); return null; });
-    Route::add('/exchange',function() { echo retornarErro(400); return null; });
-    Route::add('/exchange/([0-9.-]+)', function() { echo retornarErro(400); return null; }, 'get');
-    Route::add('/exchange/([0-9.-]+)/', function() { echo retornarErro(400); return null; }, 'get');
-    Route::add('/exchange/([0-9.-]+)/([A-Z]+)', function() { echo retornarErro(400); return null; }, 'get');
-    Route::add('/exchange/([0-9.-]+)/([A-Z]+)/([A-Z]+)', function() { echo retornarErro(400); return null; }, 'get');
-    Route::add('/exchange/([a-z])/([A-Z]+)/([A-Z]+)/([0-9.]+)', function() { echo retornarErro(400); return null; }, 'get');
-    Route::add('/exchange/([0-9.]+)/([A-Z]+)/([A-Z]+)/([a-z])', function() { echo retornarErro(400); return null; }, 'get');
-    Route::add('/exchange/(-[0-9]+)/([A-Z]+)/([A-Z]+)/([0-9.]+)', function() { echo retornarErro(400); return null; }, 'get');
-    Route::add('/exchange/([0-9.]+)/([A-Z]+)/([A-Z]+)/(-[0-9.]+)', function() { echo retornarErro(400); return null; }, 'get');
-    Route::add('/exchange/([0-9]+)/([a-z]+)/([A-Z]+)/([0-9.]+)', function() { echo retornarErro(400); return null; }, 'get');
-    Route::add('/exchange/([0-9]+)/([A-Z]+)/([a-z]+)/([0-9.]+)', function() { echo retornarErro(400); return null; }, 'get');  
+    Route::add('/',function() { retornarErro(); return null; });
+    Route::add('/exchange',function() {  retornarErro(); return null; });
+    Route::add('/exchange/([0-9.-]+)', function() { retornarErro(); return null; }, 'get');
+    Route::add('/exchange/([0-9.-]+)/', function() { retornarErro(); return null; }, 'get');
+    Route::add('/exchange/([0-9.-]+)/([A-Z]+)', function() { retornarErro(); return null; }, 'get');
+    Route::add('/exchange/([0-9.-]+)/([A-Z]+)/([A-Z]+)', function() { retornarErro(); return null; }, 'get');
+    Route::add('/exchange/([a-z])/([A-Z]+)/([A-Z]+)/([0-9.]+)', function() { retornarErro(); return null; }, 'get');
+    Route::add('/exchange/([0-9.]+)/([A-Z]+)/([A-Z]+)/([a-z])', function() { retornarErro(); return null; }, 'get');
+    Route::add('/exchange/(-[0-9]+)/([A-Z]+)/([A-Z]+)/([0-9.]+)', function() { retornarErro(); return null; }, 'get');
+    Route::add('/exchange/([0-9.]+)/([A-Z]+)/([A-Z]+)/(-[0-9.]+)', function() { retornarErro(); return null; }, 'get');
+    Route::add('/exchange/([0-9]+)/([a-z]+)/([A-Z]+)/([0-9.]+)', function() { retornarErro(); return null; }, 'get');
+    Route::add('/exchange/([0-9]+)/([A-Z]+)/([a-z]+)/([0-9.]+)', function() { retornarErro(); return null; }, 'get');  
     /***final da validação***/
 
     /***se todas as condições forem satisfeitas***/
@@ -54,21 +54,16 @@
 
 Route::run('/');
 
-    function retornarErro($erro)
+    function retornarErro()
     {
 
+        $erro = 400;
         header_remove();
-
         http_response_code($erro);
-
         header("Cache-Control: no-transform,public,max-age=300,s-maxage=900");
-
         header('Content-Type: application/json');
-
-        $status = array(200 => '200 OK',400 => '400 Bad Request',422 => 'Unprocessable Entity',500 => '500 Internal Server Error');
-
-        header("HTTP/1.1 ".$status[$erro]);
+        header("HTTP/1.1 "."400 Bad Request");
         
-        $json_response = json_encode($status[$erro], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+        $json_response = json_encode("400 Bad Request", JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
         echo $json_response;
     }
