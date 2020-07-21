d353308c593f92a75954103eb5f958131bd9c078
Adler de Oliveira <50225198+adlerdeoliveira@users.noreply.github.com>
2020-07-20 21:31:13 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -19,7 +19,22 @@
 
     require __DIR__ . '/vendor/autoload.php';
 
-    Route::add('/exchange/([0-9.-]+)/([A-Z]+)/([A-Z]+)/([0-9.-]+)', function($amount, $from, $to, $rate) {
+    //Validação de erros de requisição na url
+    Route::add('/',function()   { echo retornarErro(400); });
+    Route::add('/exchange',function()   { echo retornarErro(400); });
+    Route::add('/exchange/([0-9.-]+)', function() { echo retornarErro(400); }, 'get');
+
+    Route::add('/exchange/([0-9.-]+)/([A-Z]+)', function() { echo retornarErro(400); }, 'get');
+    Route::add('/exchange/([0-9.-]+)/([A-Z]+)/([A-Z]+)', function() { echo retornarErro(400); }, 'get');
+    Route::add('/exchange/([a-z])/([A-Z]+)/([A-Z]+)/([0-9.]+)', function()      { echo retornarErro(400); }, 'get');
+    Route::add('/exchange/([0-9.]+)/([A-Z]+)/([A-Z]+)/([a-z])', function()      { echo retornarErro(400); }, 'get');
+    Route::add('/exchange/(-[0-9]+)/([A-Z]+)/([A-Z]+)/([0-9.]+)', function()    { echo retornarErro(400); }, 'get');
+    Route::add('/exchange/([0-9.]+)/([A-Z]+)/([A-Z]+)/(-[0-9.]+)', function()   { echo retornarErro(400); }, 'get');
+    Route::add('/exchange/([0-9]+)/([a-z]+)/([A-Z]+)/([0-9.]+)', function()     { echo retornarErro(400); }, 'get');
+    Route::add('/exchange/([0-9]+)/([A-Z]+)/([a-z]+)/([0-9.]+)', function()     { echo retornarErro(400); }, 'get');  
+    /***final da validação***/  
+
+    Route::add('/exchange/([0-9.]+)/([A-Z]+)/([A-Z]+)/([0-9.]+)', function($amount, $from, $to, $rate) {
     
     $simboloMoeda    = array('BRL' => 'R$', 'USD' => '$', 'EUR' => '€');
     $amount          = str_replace(',', '.',  $amount);
@@ -27,12 +42,35 @@
  
     $valorConvertido = round($amount * $rate,2) ;                    
     $resultado       = array("valorConvertido"=>$valorConvertido,"simboloMoeda"=>$simboloMoeda);
-        
-    //$resultado  = ['valorConvertido' => $valorConvertido, 'simboloMoeda' => $simboloMoeda[$to]];
- 
+
     $retorno = json_encode(['valorConvertido' => $valorConvertido, 'simboloMoeda' => $simboloMoeda[$to]]);
     echo($retorno);
     
 }, 'get');
 
 Route::run('/');
+
+
+function retornarErro($code)
+{
+
+    header_remove();
+
+    http_response_code($code);
+
+    header("Cache-Control: no-transform,public,max-age=300,s-maxage=900");
+
+    header('Content-Type: application/json');
+
+    $status = array(
+        200 => '200 OK',
+        400 => '400 Bad Request',
+        422 => 'Unprocessable Entity',
+        500 => '500 Internal Server Error'
+        );
+
+    header("HTTP/1.1 ".$status[$code]);
+    
+    $json_response = json_encode($status[$code], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+    echo $json_response;
+}
