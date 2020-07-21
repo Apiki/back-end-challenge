918a5c045f50aa4d82984fd15aea687b347c1142
Adler de Oliveira <50225198+adlerdeoliveira@users.noreply.github.com>
2020-07-20 21:52:46 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -14,37 +14,37 @@
  * @link     https://github.com/apiki/back-end-challenge
  */
 
-    declare (strict_types = 1);
+    declare (strict_types=1);
     require 'class/Router.php';
 
     require __DIR__ . '/vendor/autoload.php';
 
     //Validação de erros de requisição na url
-    Route::add('/',function()   { echo retornarErro(400); });
-    Route::add('/exchange',function()   { echo retornarErro(400); });
-    Route::add('/exchange/([0-9.-]+)', function() { echo retornarErro(400); }, 'get');
+    Route::add('',function() { echo retornarErro(400); return null; });
+    Route::add('/',function() { echo retornarErro(400); return null; });
+    Route::add('/exchange',function() { echo retornarErro(400); return null; });
+    Route::add('/exchange/([0-9.-]+)', function() { echo retornarErro(400); return null; }, 'get');
 
-    Route::add('/exchange/([0-9.-]+)/([A-Z]+)', function() { echo retornarErro(400); }, 'get');
-    Route::add('/exchange/([0-9.-]+)/([A-Z]+)/([A-Z]+)', function() { echo retornarErro(400); }, 'get');
-    Route::add('/exchange/([a-z])/([A-Z]+)/([A-Z]+)/([0-9.]+)', function()      { echo retornarErro(400); }, 'get');
-    Route::add('/exchange/([0-9.]+)/([A-Z]+)/([A-Z]+)/([a-z])', function()      { echo retornarErro(400); }, 'get');
-    Route::add('/exchange/(-[0-9]+)/([A-Z]+)/([A-Z]+)/([0-9.]+)', function()    { echo retornarErro(400); }, 'get');
-    Route::add('/exchange/([0-9.]+)/([A-Z]+)/([A-Z]+)/(-[0-9.]+)', function()   { echo retornarErro(400); }, 'get');
-    Route::add('/exchange/([0-9]+)/([a-z]+)/([A-Z]+)/([0-9.]+)', function()     { echo retornarErro(400); }, 'get');
-    Route::add('/exchange/([0-9]+)/([A-Z]+)/([a-z]+)/([0-9.]+)', function()     { echo retornarErro(400); }, 'get');  
+    Route::add('/exchange/([0-9.-]+)/([A-Z]+)', function() { echo retornarErro(400); return null; }, 'get');
+    Route::add('/exchange/([0-9.-]+)/([A-Z]+)/([A-Z]+)', function() { echo retornarErro(400); return null; }, 'get');
+    Route::add('/exchange/([a-z])/([A-Z]+)/([A-Z]+)/([0-9.]+)', function() { echo retornarErro(400); return null; }, 'get');
+    Route::add('/exchange/([0-9.]+)/([A-Z]+)/([A-Z]+)/([a-z])', function() { echo retornarErro(400); return null; }, 'get');
+    Route::add('/exchange/(-[0-9]+)/([A-Z]+)/([A-Z]+)/([0-9.]+)', function() { echo retornarErro(400); return null; }, 'get');
+    Route::add('/exchange/([0-9.]+)/([A-Z]+)/([A-Z]+)/(-[0-9.]+)', function() { echo retornarErro(400); return null; }, 'get');
+    Route::add('/exchange/([0-9]+)/([a-z]+)/([A-Z]+)/([0-9.]+)', function() { echo retornarErro(400); return null; }, 'get');
+    Route::add('/exchange/([0-9]+)/([A-Z]+)/([a-z]+)/([0-9.]+)', function() { echo retornarErro(400); return null; }, 'get');  
     /***final da validação***/  
 
     Route::add('/exchange/([0-9.]+)/([A-Z]+)/([A-Z]+)/([0-9.]+)', function($amount, $from, $to, $rate) {
     
-    $simboloMoeda    = array('BRL' => 'R$', 'USD' => '$', 'EUR' => '€');
-    $amount          = str_replace(',', '.',  $amount);
-    $rate            = str_replace(',', '.',  $rate);
+    $simboloMoeda=array('BRL' => 'R$', 'USD' => '$', 'EUR' => '€');
+    $amount=str_replace(',', '.',  $amount);
+    $rate=str_replace(',', '.',  $rate);
  
     $valorConvertido = round($amount * $rate,2) ;                    
-    $resultado       = array("valorConvertido"=>$valorConvertido,"simboloMoeda"=>$simboloMoeda);
 
-    $retorno = json_encode(['valorConvertido' => $valorConvertido, 'simboloMoeda' => $simboloMoeda[$to]]);
-    echo($retorno);
+    $resultado = json_encode(['valorConvertido' => $valorConvertido, 'simboloMoeda' => $simboloMoeda[$to]]);
+    echo($resultado);
     
 }, 'get');
 
