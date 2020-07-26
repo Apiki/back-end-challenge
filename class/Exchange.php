a54aae72a2ee5f9cb5208bf5d4584c67b17af5cc
Adler de Oliveira <50225198+adlerdeoliveira@users.noreply.github.com>
2020-07-26 03:50:42 -0300

    Add files via upload

--- class/Exchange.php
+++ class/Exchange.php
@@ -16,19 +16,14 @@ class Exchange
 		$to 		= $parametros[4];	//Parâmetro - Moeda de desejada
 		$rate 		= $parametros[5];	//Parâmetro - Taxa da moeda desejada
 
-		$amount	 = str_replace('.', '', $amount);
-		$amount	 = str_replace(',', '.', $amount);
-		$rate	 = str_replace('.', '', $rate);
-		$rate	 = str_replace(',', '.', $rate);
-
-		$valorConvertido = round($amount * $rate,2) ;                    
+	  	$valorConvertido = (string)(float)$amount * (string)(float)$rate;
+	  	$valorConvertido = (string)(float)$valorConvertido;
 		
 		if ($valorConvertido >0) {
 			
 			$resultado = json_encode(['valorConvertido' => $valorConvertido, 'simboloMoeda' => $simboloMoeda[$to]]);
 
 		} else {	
-
 
 		        $erro = 400;
 		        header_remove();
