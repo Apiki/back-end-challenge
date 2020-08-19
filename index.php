e1e3635df74045cea9428cc8b79e5f8996b64865
Carlos H. Araújo <carloshaam@gmail.com>
2020-08-19 16:02:57 -0300

    teste

--- index.php
+++ index.php
@@ -8,10 +8,39 @@
  *
  * @category Challenge
  * @package  Back-end
- * @author   Seu Nome <seu-email@seu-provedor.com>
+ * @author   Carlos H. Aráujo <carloshaam@gmail.com>
  * @license  http://opensource.org/licenses/MIT MIT
  * @link     https://github.com/apiki/back-end-challenge
  */
 declare(strict_types=1);
 
 require __DIR__ . '/vendor/autoload.php';
+
+$dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) {
+    $r->addRoute('GET', '/exchange/{amount}/{from}/{to}/{rate}', 'App\\Exchange/convert');
+});
+
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+$uri = $_SERVER['REQUEST_URI'];
+
+if (false !== $pos = strpos($uri, '?')) {
+    $uri = substr($uri, 0, $pos);
+}
+$uri = rawurldecode($uri);
+
+$routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+switch ($routeInfo[0]) {
+    case FastRoute\Dispatcher::NOT_FOUND:
+        http_response_code(404);
+        break;
+    case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+        $allowedMethods = $routeInfo[1];
+        http_response_code(405);
+        break;
+    case FastRoute\Dispatcher::FOUND:
+        $handler = $routeInfo[1];
+        $vars = $routeInfo[2];
+        list($class, $method) = explode("/", $handler, 2);
+        call_user_func_array(array(new $class, $method), $vars);
+        break;
+}
---
+++ src/Exchange.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App;
+
+class Exchange extends Base
+{
+    private $coins = ["BRL", "USD", "EUR"];
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function convert($amount, $from, $to, $rate): void
+    {
+        if(in_array($from, $this->coins) && in_array($to, $this->coins) && $this->checkValue($amount) && $this->checkValue($rate)){
+            $response = [
+                'valorConvertido' => $this->calculateExchange($amount, $rate), 'simboloMoeda' => $this->backSymbol($to)
+            ];
+            $this->back($response);
+        }else{
+            $this->call(
+                404,
+                "not_found",
+                "Valor incorreto"
+            )->back();
+            return;
+        }
+    }
+
+    public function backSymbol($to)
+    {
+        switch ($to){
+            case "BRL":
+                return "R$";
+            case "USD":
+                return "$";
+            case "EUR":
+                return "€";
+        }
+    }
+
+    public function checkValue($value)
+    {
+        if(preg_match('/^(\d+(\.\d+)?)$/', $value) > 0 && $value > 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public function calculateExchange($amount, $rate)
+    {
+        return ($amount * $rate);
+    }
+
+}
