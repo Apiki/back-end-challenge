c232de2a8e17e814a5957e13e7d6702cf60fc686
unknown <analitico01@gmail.com>
2021-01-14 16:55:10 -0300

    teste concluido

--- index.php
+++ index.php
@@ -1,17 +1,65 @@
 <?php
-/**
- * Back-end Challenge.
- *
- * PHP version 7.2
- *
- * Este será o arquivo chamado na execução dos testes automátizados.
- *
- * @category Challenge
- * @package  Back-end
- * @author   Seu Nome <seu-email@seu-provedor.com>
- * @license  http://opensource.org/licenses/MIT MIT
- * @link     https://github.com/apiki/back-end-challenge
- */
-declare(strict_types=1);
 
-require __DIR__ . '/vendor/autoload.php';
+require 'vendor/autoload.php';
+
+use Buki\Router\Router;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+$router = new Router;
+
+
+
+// For basic GET URI
+$router->get('/:all', function() {
+
+    
+$originalURI = trim($_SERVER['REQUEST_URI'], '/exchange');
+
+    $converion = explode('/', $originalURI);
+
+    if(count($converion) != 4) {
+
+        echo "Faltam parâmetros!";
+
+    } else {
+
+        $value = $converion[0];
+        if($value <= 0) {
+            $arr = array("valorAconverter" => "Menor ou igual a 0");
+            return json_encode($arr);
+            // return http_response_code(400);
+        }
+        $from = strtoupper($converion[1]);
+        $to = strtoupper($converion[2]);
+        $rate = $converion[3];
+
+        $resultValue = $value * $rate;
+
+        switch($to) {
+            case "BRL":
+                $resultSymbol = "R$";
+                break;
+            case "USD":
+                $resultSymbol = "$";
+                break;
+            case "EUR":
+                $resultSymbol = "€";
+                break;
+        }
+        
+
+        $array = array('valorConvertido' => round($resultValue, 2), 'simboloMoeda' => $resultSymbol);
+        
+        return json_encode($array);
+    }
+
+});
+
+
+
+$router->run();
+
+
+
+
