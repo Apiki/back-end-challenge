2162530ba29240330868161a0703779d6af7651f
Edy Wladson <edywladson@gmail.com>
2021-01-18 18:24:33 -0300

    Criado classes, controllers, views e rotas da api

--- index.php
+++ index.php
@@ -15,3 +15,37 @@
 declare(strict_types=1);
 
 require __DIR__ . '/vendor/autoload.php';
+
+// BOOTSTRAP
+
+use CoffeeCode\Router\Router;
+
+/**
+ * API ROUTES
+ * index.
+ */
+$route = new Router("http://localhost:8000", ':');
+$route->namespace('App\Controllers\Api');
+
+//exchange
+$route->group('/exchange');
+$route->get('/', 'Exchange:index');
+$route->get('/{amount}/{from}/{to}/{rate}', 'Exchange:index');
+
+// ROUTE
+$route->dispatch();
+
+// ERROR REDIRECT
+if ($route->error()) {
+    header('Content-Type: application/json; charset=UTF-8');
+    http_response_code(400);
+
+    echo json_encode([
+        'errors' => [
+            'type ' => 'invalid_data',
+            'message' => 'Favor informe os dados corretamente',
+        ],
+    ], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+}
+
+ob_end_flush();
---
+++ src/Controllers/Api/Exchange.php
@@ -0,0 +1,49 @@
+<?php
+
+
+namespace App\Controllers\Api;
+
+
+use App\Support\Currency;
+
+class Exchange extends Api
+{
+    /**
+     * __construct.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * index
+     */
+    public function index(array $data): void
+    {
+        $data = filter_var_array($data, FILTER_SANITIZE_STRIPPED);
+
+        if (empty($data)) {
+            $this->call(
+                400,
+                'invalid_data',
+                'Favor informe os dados corretamente'
+            )->back();
+
+            return;
+        }
+
+        $currency = new Currency();
+        if (!$currency->verify($data)) {
+            $this->call(
+                400,
+                'invalid_data',
+                'Favor informe os dados corretamente'
+            )->back();
+
+            return;
+        }
+
+        $this->back($currency->convert($data));
+    }
+}
