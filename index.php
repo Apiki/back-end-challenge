9179b07d8a73bffde95945e7ee06e668c96b0e11
Fred Davies <67489481+DaviesDeveloper@users.noreply.github.com>
2021-01-25 08:19:53 -0300

    Update index.php

--- index.php
+++ index.php
@@ -15,3 +15,54 @@
 declare(strict_types=1);
 
 require __DIR__ . '/vendor/autoload.php';
+
+class ClasseConversao {
+		public function ConverterValores(){
+			//return "teste";
+			$amount = 0;
+	
+			$rate = 0;
+
+			$simbolo = "";
+			
+			$to = "";
+			
+			if (isset($_GET['amount'])){
+				$amount = $_GET['amount'];                          //Parâmetro - Quantia	
+			}
+			if (isset($_GET['rate'])){
+				$rate = $_GET['rate'];                              //Parâmetro - Moeda de desejada
+			}
+
+			$total = $amount * $rate;                           //Parâmetro - Taxa da moeda desejada
+			$total = number_format($total, 2, ',', '.');        //Parâmetro - Resultado
+            $simbolo = '-';                                     //Parâmetro - Símbolo da moeda desejado
+			
+			switch( $to ){
+				case 'USD':
+					$simbolo = '$';
+					break;
+				case 'BRL':
+					$simbolo = 'R$';
+					break;
+				case 'EUR':
+					$simbolo = '€';
+					break;
+			}
+
+			$vetor = array(
+					'valorConvertido' => $total, 
+					'simboloMoeda' => $simbolo
+				);
+
+			return json_encode($vetor);
+		}
+	}
+	
+	$fred = new ClasseConversao();
+	
+	$meu_json = $fred->ConverterValores();
+?>
+	<pre>
+		<?php var_dump($meu_json); ?>
+	</pre>
