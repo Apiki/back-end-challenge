4df332770184c4dc748f2e06fceb23cb207717ec
Fred Davies <67489481+daviesdesenvolvedor@users.noreply.github.com>
2021-01-27 17:42:04 -0300

    Update index.php

--- index.php
+++ index.php
@@ -16,38 +16,49 @@ declare(strict_types=1);
 
 require __DIR__ . '/vendor/autoload.php';
 
-class ClasseConversao {
+
+	class minhaClasse {
 		public function ConverterValores(){
-			//return "teste";
+			//DECLARAÇÃO E INICIALIZAÇÃO DE VARIÁVEIS
 			$amount = 0;
-	
-			$rate = 0;
-
-			$simbolo = "";
-			
+			$from = "";
 			$to = "";
+			$rate = 0;
+			$total = 0;
+			$simbolo = '-';
 			
-			if (isset($_GET['amount'])){
-				$amount = $_GET['amount'];                          //Parâmetro - Quantia	
+			//CARREGAR VALORES
+			if ( isset($_GET['amount']) ) {
+				$amount = $_GET['amount'];
 			}
-			if (isset($_GET['rate'])){
-				$rate = $_GET['rate'];                              //Parâmetro - Moeda de desejada
+			if ( isset($_GET['from']) ) {
+				$from = $_GET['from'];
 			}
-
-			$total = $amount * $rate;                           //Parâmetro - Taxa da moeda desejada
-			$total = number_format($total, 2, ',', '.');        //Parâmetro - Resultado
-            $simbolo = '-';                                     //Parâmetro - Símbolo da moeda desejado
+			if ( isset($_GET['to']) ) {
+				$to = $_GET['to'];
+			}
+			if ( isset($_GET['rate']) ) {
+				$rate = $_GET['rate'];
+			}
 			
-			switch( $to ){
-				case 'USD':
-					$simbolo = '$';
-					break;
-				case 'BRL':
-					$simbolo = 'R$';
-					break;
-				case 'EUR':
-					$simbolo = '€';
-					break;
+			if ( $amount > 0 && $rate > 0 ) {
+				$total = $amount * $rate;
+				$total = number_format($total, 2, ',', '.');
+			}
+			
+			//DEFINIR SÍMBOLO DA MOEDA
+			if ( $to != "" ) {
+				switch( $to ){
+					case 'USD':
+						$simbolo = '$';
+						break;
+					case 'BRL':
+						$simbolo = 'R$';
+						break;
+					case 'EUR':
+						$simbolo = '€';
+						break;
+				}
 			}
 
 			$vetor = array(
@@ -59,12 +70,8 @@ class ClasseConversao {
 		}
 	}
 	
-	$conv = new ClasseConversao();
+	$fred = new minhaClasse();
 	
-	$meu_json = $conv->ConverterValores();
+	$meu_json = $fred->ConverterValores();
 ?>
-	<pre>
-		<?php var_dump($meu_json); ?>
-	</pre>
-
-   
+<?php echo $meu_json; ?>
