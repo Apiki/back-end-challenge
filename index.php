6af0f83457bee462bac58ac85a5198920e5ce09c
Fred Davies <67489481+daviesdesenvolvedor@users.noreply.github.com>
2021-01-27 17:48:33 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,64 +1,39 @@
+
 <?php
-/**
- * Back-end Challenge.
- *
- * PHP version 7.2
- *
- * Este será o arquivo chamado na execução dos testes automátizados.
- *
- * @category Challenge
- * @package  Back-end
- * @author   Seu Nome <seu-email@seu-provedor.com>
- * @license  http://opensource.org/licenses/MIT MIT
- * @link     https://github.com/apiki/back-end-challenge
- */
-declare(strict_types=1);
 
-require __DIR__ . '/vendor/autoload.php';
+/* http://localhost:8000/exchange/10/BRL/USD/4.50
 
+...deve ser traduzido pelo servidor como:
+http://localhost/exchange/index.php?amount=10&from=BRL&to=USD&rate=4.50
 
-	class minhaClasse {
+A conversão é feita pelo arquivo .htacces.
+Conteúdo do .htaccess:
+
+RewriteEngine On
+RewriteCond %{REQUEST_FILENAME} !-f
+RewriteCond %{REQUEST_FILENAME} !-d
+RewriteRule ^exchange/(.)/(.)/(.)/(.)$ index.php?amount=$1&from=$2&to=$3&rate=$4 [L,QSA]
+*/
+
+	class ClasseConversao {
 		public function ConverterValores(){
-			//DECLARAÇÃO E INICIALIZAÇÃO DE VARIÁVEIS
-			$amount = 0;
-			$from = "";
-			$to = "";
-			$rate = 0;
-			$total = 0;
-			$simbolo = '-';
+
+            $amount = $_GET['amount'];                          //Parâmetro - Quantia
+			$rate = $_GET['rate'];                              //Parâmetro - Moeda de desejada
+			$total = $amount * $rate;                           //Parâmetro - Taxa da moeda desejada
+			$total = number_format($total, 2, ',', '.');        //Parâmetro - Resultado
+            $simbolo = '-';                                     //Parâmetro - Símbolo da moeda desejado
 			
-			//CARREGAR VALORES
-			if ( isset($_GET['amount']) ) {
-				$amount = $_GET['amount'];
-			}
-			if ( isset($_GET['from']) ) {
-				$from = $_GET['from'];
-			}
-			if ( isset($_GET['to']) ) {
-				$to = $_GET['to'];
-			}
-			if ( isset($_GET['rate']) ) {
-				$rate = $_GET['rate'];
-			}
-			
-			if ( $amount > 0 && $rate > 0 ) {
-				$total = $amount * $rate;
-				$total = number_format($total, 2, ',', '.');
-			}
-			
-			//DEFINIR SÍMBOLO DA MOEDA
-			if ( $to != "" ) {
-				switch( $to ){
-					case 'USD':
-						$simbolo = '$';
-						break;
-					case 'BRL':
-						$simbolo = 'R$';
-						break;
-					case 'EUR':
-						$simbolo = '€';
-						break;
-				}
+			switch( $to ){
+				case 'USD':
+					$simbolo = '$';
+					break;
+				case 'BRL':
+					$simbolo = 'R$';
+					break;
+				case 'EUR':
+					$simbolo = '€';
+					break;
 			}
 
 			$vetor = array(
@@ -70,8 +45,12 @@ require __DIR__ . '/vendor/autoload.php';
 		}
 	}
 	
-	$fred = new minhaClasse();
+	$fred = new ClasseConversao();
 	
 	$meu_json = $fred->ConverterValores();
 ?>
-<?php echo $meu_json; ?>
+	<pre>
+		<?php var_dump($meu_json); ?>
+	</pre>
+
+   
