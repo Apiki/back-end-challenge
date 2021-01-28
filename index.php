d2df2a2fec78894a475291711f62c5502c2ca219
Fred Davies <67489481+daviesdesenvolvedor@users.noreply.github.com>
2021-01-28 16:54:04 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,25 @@
 
-
 <?php
-
+/**
+ * Back-end Challenge.
+ *
+ * PHP version 7.2
+ *
+ * Este será o arquivo chamado na execução dos testes automátizados.
+ *
+ * @category Challenge
+ * @package  Back-end
+ * @author   Seu Nome <seu-email@seu-provedor.com>
+ * @license  http://opensource.org/licenses/MIT MIT
+ * @link     https://github.com/apiki/back-end-challenge
+ */
 declare(strict_types=1);
 
 require __DIR__ . '/vendor/autoload.php';
+
+?>
+
+<?php
 
 
 	class exchange {
