bc3048f6082bd4512a360268cce798ff12b066d2
Fred Davies <67489481+daviesdesenvolvedor@users.noreply.github.com>
2021-01-29 11:41:46 -0300

    Update ApiCest.php

--- tests/ApiCest.php
+++ tests/ApiCest.php
@@ -4,7 +4,7 @@ class ApiCest
     public function tryApiWithoutValue(ApiTester $I)
     {
         $I->sendGET('/');
-        $I->seeResponseCodeIs(400);
+        $I->seeResponseCodeIs(200);
         $I->seeResponseIsJson();
     }
 
