d82036114b6f0424d4a2fd844f460ed3e8b08cc3
Fred Davies <67489481+daviesdesenvolvedor@users.noreply.github.com>
2021-01-29 08:13:35 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,79 +1,163 @@
+
 <?php
-/**
- * Back-end Challenge.
- *
- * PHP version 7.2
- *
- * Este será o arquivo chamado na execução dos testes automátizados.
- *
- * @category Challenge
- * @package  Back-end
- * @author   Seu Nome <seu-email@seu-provedor.com>
- * @license  http://opensource.org/licenses/MIT MIT
- * @link     https://github.com/apiki/back-end-challenge
- */
-declare(strict_types=1);
 
-require __DIR__ . '/vendor/autoload.php';
+final class Validacoes
+{
+    const ROTA = 'exchange';
+    const QTD_PARAMETROS = 4;
+    const currencyes = array('BRL', 'EUR', 'USD');
+
+    var $amount;
+    var $from;
+    var $to;
+    var $rate;
+
+    var $messages = array();
+
+    private function getURI()
+    {
+        $rota = explode("/", parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+        return $rota;
+    }
+
+    private function validarRota()
+    {
+        if ($this->getURI()[1] == self::ROTA) {
+            return TRUE;
+        } else {
+            array_push($this->messages, 'Rota invalida');
+            return FALSE;
+        }
+    }
+
+    private function validarQtdParametros()
+    {
+        if ($this->validarRota()) {
+            //Somo com 2 porque o primeiro parâmetro é vazio e o segundo é a rota
+            if (count($this->getURI()) == 2 + self::QTD_PARAMETROS) {
+                return TRUE;
+            } else {
+                array_push($this->messages, 'Quantidade de parametros incorreta');
+                return FALSE;
+            }
+        }
+        return FALSE;
+    }
+
+    private function validarTipoParametros()
+    {
+        if ($this->validarQtdParametros()) {
+            $parametros = $this->getURI();
+            $this->amount = $parametros[2];
+            $this->from = $parametros[3];
+            $this->to = $parametros[4];
+            $this->rate = $parametros[5];
 
 
-	class exchange {
-		public function converter_valores(){
-			//CHECAR SE OS PARAMETROS FORAM PASSADOS
-			if ( !isset($_GET['amount']) ) $this->abort();
-			if ( !isset($_GET['from']) ) $this->abort();
-			if ( !isset($_GET['to']) ) $this->abort();
-			if ( !isset($_GET['rate']) ) $this->abort();
-			
-			//CARREGAR OS PARAMETROS NAS VARIÁVEIS
-			$amount	= $_GET['amount'];
-			$from 	= $_GET['from'];
-			$to 	= $_GET['to'];
-			$rate 	= $_GET['rate'];
-			
-			//VALIDAR VALORES
-			if ( !is_numeric($amount) ) $this->abort();
-			if ( !is_numeric($rate) ) $this->abort();
-			if ( strlen($from) != 3 ) $this->abort();
-			if ( strlen($to) != 3 ) $this->abort();
-			
-			
-			//CALCULAR O TOTAL
-			$total = $amount * $rate;
-			$total = number_format($total, 2, ',', '.');
-			
-			//DEFINIR SÍMBOLO DA MOEDA
-			$simbolo = '-';
-			if ( $to != "" ) {
-				switch( $to ){
-					case 'USD':
-						$simbolo = '$';
-						break;
-					case 'BRL':
-						$simbolo = 'R$';
-						break;
-					case 'EUR':
-						$simbolo = '€';
-						break;
-				}
-			}
+            //Valida se o AMOUNT é um número
+            if (!is_numeric($this->amount)) {
+                array_push($this->messages, 'Valor de AMOUNT nao e numerico');
+                return FALSE;
+            }
 
-			$vetor = array(
-					'valorConvertido' => $total, 
-					'simboloMoeda' => $simbolo
-				);
+            //Valida se o AMOUNT é positivo
+            if (floatval($this->amount) * 1 < 0) {
+                array_push($this->messages, 'Valor de AMOUNT nao e positivo');
+                return FALSE;
+            }
 
-			return json_encode($vetor);
-		}
-		
-		function abort() {
-			echo '400';
-			die();
-		}
-	}
-	
-	$obj_exchange = new exchange();
-	
-	$meu_json = $obj_exchange->converter_valores();
-?>
-<?php echo $meu_json; ?>
+            //Valida se o FROM está dentro das moedas definidas
+            if (!in_array($this->from, self::currencyes, TRUE)) {
+                array_push($this->messages, 'Valor de FROM nao esta entre as moedas aceitas');
+                return FALSE;
+            }
+
+            //Valida se o TO está dentro das moedas definidas
+            if (!in_array($this->to, self::currencyes, TRUE)) {
+                array_push($this->messages, 'Valor de TO nao esta entre as moedas aceitas');
+                return FALSE;
+            }
+
+            //Valida se o RATE é um número
+            if (!is_numeric($this->rate)) {
+                array_push($this->messages, 'Valor de RATE nao e numerico');
+                return FALSE;
+            }
+
+            //Valida se o RATE é positivo
+            if (floatval($this->rate) * 1 < 0) {
+                array_push($this->messages, 'Valor de RATE nao e positivo');
+                return FALSE;
+            }
+
+            return TRUE;
+        }
+        return FALSE;
+    }
+
+    public function validar()
+    {
+        return $this->validarTipoParametros();
+    }
+}
+
+class App
+{
+    var $saida = array(
+        'valid' => true,
+        'code' => 200,
+        'message' => 'Success'
+    );
+    var $validacoes;
+
+    public function run()
+    {
+        $this->validacoes = new Validacoes();
+        $retorno = FALSE;
+
+        if ($this->validacoes->validar()) {
+            $resultado = $this->calcular($this->validacoes->amount, $this->validacoes->rate);
+            $moeda = '';
+            switch ($this->validacoes->to) {
+                case 'USD':
+                    $moeda = '$';
+                    break;
+                case 'EUR':
+                    $moeda = '€';
+                    break;
+                case 'BRL':
+                    $moeda = 'R$';
+                    break;
+            }
+
+            $retorno = array(
+                'valorConvertido' => $resultado,
+                'simboloMoeda' => $moeda
+            );
+        }
+
+        $this->retornar($retorno);
+    }
+
+    private function calcular($a, $b)
+    {
+        return $a * $b;
+    }
+
+    private function retornar($r)
+    {
+        if (!$r){
+            $this->saida = array(
+                'valid' => false,
+                'code' => 400,
+                'message' => array_pop($this->validacoes->messages)
+            );
+            header('Content-Type: application/json; charset=utf-8', true, 400);
+        } else {
+            $this->saida = array_merge($this->saida, $r);
+            header('Content-Type: application/json; charset=utf-8', true, 200);            
+        }
+        echo json_encode($this->saida, JSON_NUMERIC_CHECK);
+    }
+
+}
