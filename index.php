c6e86031a1a73dd4a151a904165c4d3600a515dc
Fred Davies <67489481+daviesdesenvolvedor@users.noreply.github.com>
2021-01-30 22:53:43 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,65 @@
-
 <?php
-/**
- * Back-end Challenge.
- *
- * PHP version 7.2
- *
- * Este será o arquivo chamado na execução dos testes automátizados.
- *
- * @category Challenge
- * @package  Back-end
- * @author   Kalil Maciel <kalilmaciel@gmail.com>
- * @license  http://opensource.org/licenses/MIT MIT
- * @link     https://github.com/apiki/back-end-challenge
- */
-declare(strict_types=1);
+	class exchange {
+		public function converter_valores(){
+			$ar = array();
+			if ( isset($_SERVER['REQUEST_URI']) ) {
+				$ar = explode("/", trim($_SERVER['REQUEST_URI'], '/'));
+				
+				//REMOVER O PARAMETRO EXCHANGE SE EXISTIR
+				if (count($ar) > 0 && $ar[0]=='exchange') $ar = array_slice($ar, 1);
+			}
+			
+			//CHECAR SE FORAM PASSADOS 4 PARAMETROS
+			if ( count($ar) != 4 ) $this->abort();
+			
+			//CARREGAR OS PARAMETROS NAS VARIÁVEIS
+			$amount	= $ar[0];
+			$from 	= $ar[1];
+			$to 	= $ar[2];
+			$rate 	= $ar[3];
+			
+			//VALIDAR VALORES
+			if ( !is_numeric($amount) ) $this->abort();
+			if ( !is_numeric($rate) ) $this->abort();
+			if ( strlen($from) != 3 ) $this->abort();
+			if ( strlen($to) != 3 ) $this->abort();
+			
+			//CALCULAR O TOTAL
+			$total = $amount * $rate;
+			$total = number_format($total, 2, ',', '.');
+			
+			//DEFINIR SÍMBOLO DA MOEDA
+			$simbolo = '-';
+			if ( $to != "" ) {
+				switch( $to ){
+					case 'USD':
+						$simbolo = '$';
+						break;
+					case 'BRL':
+						$simbolo = 'R$';
+						break;
+					case 'EUR':
+						$simbolo = '€';
+						break;
+				}
+			}
 
-/*require __DIR__ . '/vendor/autoload.php';*/
+			$vetor = array(
+					'valorConvertido' => $total, 
+					'simboloMoeda' => $simbolo
+				);
 
-chdir(__DIR__ . '/exchange/');
-
-include_once __DIR__.'/exchange/index.php';
+			return json_encode($vetor);
+		}
+		
+		function abort() {
+			echo '400';
+			die();
+		}
+	}
+	
+	$obj_exchange = new exchange();
+	
+	$meu_json = $obj_exchange->converter_valores();
+?>
+<?php echo $meu_json; ?>
