756bf0b4b961c3fb0b77b2851798d146d680c18b
unknown <adm@IV3508.alianca.local>
2021-02-03 21:55:56 -0300

    Do back end challenge

--- index.php
+++ index.php
@@ -15,3 +15,7 @@
 declare(strict_types=1);
 
 require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/src/exchange-class.php';
+
+$request = $_SERVER['REQUEST_URI'];
+$exchange = new ExchangeClass( $request );
