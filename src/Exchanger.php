9cf038a9d388f71a512af2f73dd3a4f566d0d1ff
Rodrigo Nascimento <me@rcnascimento.com>
2021-04-02 16:24:45 -0300

    Iniciando

---
+++ src/Exchanger.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * API REST para conversão de moedas.
+ *
+ * PHP version 7.2
+ *
+ * @category Challenge
+ * @package  Back-end
+ * @author   Rodrigo Nascimento <me@rcnascimento.com>
+ * @license  http://opensource.org/licenses/MIT MIT
+ * @link     https://github.com/apiki/back-end-challenge
+ */
+
+namespace App;
+
+/**
+ * Conversos básico de algumas moedas.
+ *
+ * @category Challenge
+ * @package  Back-end
+ * @author   Rodrigo Nascimento <me@rcnascimento.com>
+ * @license  http://opensource.org/licenses/MIT MIT
+ * @link     https://github.com/apiki/back-end-challenge
+ */
+class Exchanger
+{
+    /**
+     * Entrypoint da aplicação.
+     *
+     * @return void
+     */
+    public function exchange()
+    {
+        echo 'Iniciando!';
+    }
+}
