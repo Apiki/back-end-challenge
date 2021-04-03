6e2c4c91ac96d2453fc517df48d22c1338407838
Rodrigo Nascimento <me@rcnascimento.com>
2021-04-03 03:46:15 -0300

    Define classe principal

--- src/Exchanger.php
+++ src/Exchanger.php
@@ -24,6 +24,62 @@ namespace App;
  */
 class Exchanger
 {
+    protected $requiredParams = [
+        'amount',
+        'from',
+        'to',
+        'rate',
+    ];
+
+    protected $responseKeys = [
+        'valorConvertido',
+        'simboloMoeda',
+    ];
+
+    protected $knownCurrencies = [
+        'BRL' => 'R$',
+        'EUR' => '€',
+        'USD' => '$',
+    ];
+
+    protected $requestParams = [];
+
+    public function __construct()
+    {
+        $this->parseRequestParams();
+    }
+
+    protected function parseRequestParams()
+    {
+        $rawRequestParams = preg_replace(
+            '/^\/([a-z]+\/)?/', '', $_SERVER['REQUEST_URI'] ?? ''
+        );
+        $requestParams    = explode('/', $rawRequestParams);
+
+        if (count($this->requiredParams) === count($requestParams)) {
+            $this->requestParams = array_combine(
+                $this->requiredParams, $requestParams
+            );
+        } else {
+            $this->requestParams = false;
+        }
+    }
+
+    protected function buildResponse(string ...$responseValues)
+    {
+        $response = [];
+
+        if (count($this->responseKeys) === count($responseValues)) {
+            $response = array_combine($this->responseKeys, $responseValues);
+        } else {
+            http_response_code(400);
+        }
+
+        header('Content-Type: application/json');
+        echo json_encode($response);
+        exit;
+    }
+
     /**
      * Entrypoint da aplicação.
      *
@@ -31,6 +87,21 @@ class Exchanger
      */
     public function exchange()
     {
-        echo 'Iniciando!';
+        if ($this->requestParams) {
+            $amount       = (float) $this->requestParams['amount'];
+            $rate         = (float) $this->requestParams['rate'];
+            $fromCurrency = $this->requestParams['from'];
+            $toCurrency   = $this->requestParams['to'];
+
+            $finalAmount    = $amount * $rate;
+            $validFrom      = $this->knownCurrencies[$fromCurrency];
+            $currencySymbol = $this->knownCurrencies[$toCurrency];
+
+            if ($finalAmount > 0 && $validFrom && $currencySymbol) {
+                $this->buildResponse($finalAmount, $currencySymbol);
+            }
+        }
+
+        $this->buildResponse();
     }
 }
