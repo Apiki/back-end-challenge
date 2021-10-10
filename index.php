5e23d12530af329da7cb1c10b2f99a2f332e876b
Rodrigo <francorodrigognr@yahoo.com.br>
2021-10-10 14:33:57 -0300

    Finalizado

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
  * @license  http://opensource.org/licenses/MIT MIT
  * @link     https://github.com/apiki/back-end-challenge
  */
-<<<<<<< HEAD
+
 declare(strict_types=1);
 
 require __DIR__ . '/vendor/autoload.php';
