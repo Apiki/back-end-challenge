7de6172ec3d9f8e56a7687e595ce217691cc9e17
Rodrigo Gaspar <rodrigogaspar@xPhreak.local>
2021-10-25 13:28:28 -0300

    RodrigoZGaspar

--- index.php
+++ index.php
@@ -8,10 +8,17 @@
  *
  * @category Challenge
  * @package  Back-end
- * @author   Seu Nome <seu-email@seu-provedor.com>
+ * @author   Rodrigo Z Gaspar <rodgasp@gmail.com>
  * @license  http://opensource.org/licenses/MIT MIT
  * @link     https://github.com/apiki/back-end-challenge
  */
 declare(strict_types=1);
 
+$_SERVER['SCRIPT_NAME'] = pathinfo(__FILE__, PATHINFO_BASENAME);
+
 require __DIR__ . '/vendor/autoload.php';
+
+include __DIR__ . "/src/exchange.php";
+
+$exchange = new Exchange();
+$exchange->Run(); 
