8516dc4bdf179528dc3f53aa1dd61c1aacae0056
eduardosteves <eduardostevesdasilva@hotmail.com>
2021-10-27 20:57:12 -0300

    O metodo load nao vai mais instanciar a classe diretamente de forma dinamica, agora a logica sera capturar e setar o metodo a ser carregado pelo controller correspondente com base na url e seus parametros e serem executados de forma dinamica pelo metodo exec() que esta encarregado de instanciar o controller, pegar o nome da action e seus parametros ja carregados e entao executa-los.

--- src/core/Controller.php
+++ src/core/Controller.php
@@ -10,6 +10,8 @@ class Controller {
 	private static $uri;
 	private $name_space;
 	const DIR_CONTROLLER = "\\App\\controllers\\";
+	private $action;
+	private $params;
 
 	/**
 	 * Retorna uma instância da classe Uri do core do micro framework com a idéia de composição.
@@ -28,7 +30,8 @@ class Controller {
 		}
 		$this->name_space = self::DIR_CONTROLLER . Controller::Uri()::getController();
 		if( class_exists($this->name_space) ){
-			return new $this->name_space();
+			$this->setAction();
+			$this->setParams();
 		}else{
 			$erro = [
 				'msg' 	=> 'Router not found',
@@ -44,6 +47,26 @@ class Controller {
 	*/
 	private function isHome() {
 		return (self::Uri()::getUri() == '/');
+	}
+
+	protected function setAction() {
+		$this->action = Controller::Uri()::getAction();
+	}
+
+	protected function setParams() {
+		$this->params = Controller::Uri()::getParams();
+	}
+
+	public function exec() {
+		$controller = new $this->name_space();
+		$action = $controller::Uri()::getAction();
+		$params = $controller::Uri()::getParams();
+		$controller->$action($params);
+	}
+
+	public function status($status){
+		header('Content-Type: application/json;charset=utf-8');
+		http_response_code($status);
 	}
 
 }
