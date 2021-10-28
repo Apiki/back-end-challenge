f23f08e01efae65ae6c67d3bbf18412d1f8e9dbd
eduardosteves <eduardostevesdasilva@hotmail.com>
2021-10-28 00:23:54 -0300

    Formatado o retorno correto para o retorno da raiz do endpoint e setado os headers correto com o usdo da funcao status() quando houver falha na validacao dos parametros

--- src/controllers/Exchange.php
+++ src/controllers/Exchange.php
@@ -7,15 +7,20 @@ use App\core\Controller;
 use App\model\BrlConvert;
 use App\components\Helpers;
 use App\model\UsdConvert;
+use App\model\EurConvert;
 
 
 class Exchange extends Controller {
 	public function index() {
-		echo "Hello World";
+		$this->status(200);
+		$msg = Helpers::msgJson('Bem vindo a conversão de moedas', 200);
+		echo $msg;
+		die;
 	}
 
 	public function brl_to_usd($datas) {
 		if(!is_array($datas)){
+			$this->status(500);
 			$msg = Helpers::msgJson('Problemas ao processar a requisição', 500);
 			throw new \Exception($msg);
 		}
@@ -30,6 +35,7 @@ class Exchange extends Controller {
 
 	public function brl_to_eur($datas) {
 		if(!is_array($datas)){
+			$this->status(500);
 			$msg = Helpers::msgJson('Problemas ao processar a requisição', 500);
 			throw new \Exception($msg);
 		}
@@ -44,6 +50,7 @@ class Exchange extends Controller {
 
 	public function usd_to_brl($datas) {
 		if(!is_array($datas)){
+			$this->status(500);
 			$msg = Helpers::msgJson('Problemas ao processar a requisição', 500);
 			throw new \Exception($msg);
 		}
@@ -51,6 +58,21 @@ class Exchange extends Controller {
 		$usd = new UsdConvert;
 		$total = $usd->convToBrl($datas['amount'], $datas['rate']);
 		$msg = Helpers::msgApi($total,$usd->simbol);
+		$this->status(200);
+		echo $msg;
+		die;
+	}
+
+	public function eur_to_brl($datas) {
+		if(!is_array($datas)){
+			$this->status(500);
+			$msg = Helpers::msgJson('Problemas ao processar a requisição', 500);
+			throw new \Exception($msg);
+		}
+
+		$eur = new EurConvert;
+		$total = $eur->convToBrl($datas['amount'], $datas['rate']);
+		$msg = Helpers::msgApi($total,$eur->simbol);
 		$this->status(200);
 		echo $msg;
 		die;
