d616548810ad484416b962b667259123be8b2855
eduardosteves <eduardostevesdasilva@hotmail.com>
2021-10-27 21:06:30 -0300

    Criado primeiro model do app que sera responsavel por converter algumas moedas de real para dollar ou euro.

---
+++ src/model/BrlConvert.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\model;
+
+
+class BrlConvert {
+
+	private $usd = 5.54;
+	private $eur = 6.42;
+	public $simbol = "r$";
+	private $total = 0.00;
+
+	public function convToUsd($rs, $usd) {
+		$this->total += ($rs/($usd * $this->usd));
+		return $this->total;
+	}
+
+}
