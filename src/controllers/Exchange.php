6bf19145bc225762fff07d904d5b9de8afe66f51
eduardosteves <eduardostevesdasilva@hotmail.com>
2021-10-27 21:04:43 -0300

    Criado o primeiro metodo referente a api brl_to_usd que vai receber um array de parametros a ser convertido pelo seu model correspondente. Normalmente os controller sao no plural e os model no singular para diferenciar bem as classes mesmo estando em namespace diferente optei por quebrar um pouco essa convencao e dar um nome mais sugestivo ao model BrlConvert.

--- src/controllers/Exchange.php
+++ src/controllers/Exchange.php
@@ -3,8 +3,35 @@
 namespace App\controllers;
 
 
-class Exchange {
+use App\core\Controller;
+use App\model\BrlConvert;
+
+
+class Exchange extends Controller {
 	public function index() {
 		echo "Hello World";
 	}
+
+	public function brl_to_usd($amount) {
+		if(!is_array($amount)){
+			$this->status(400);
+			$erro = [
+				'msg' 	=> 'Router not found',
+				'code' 	=> 404
+			];
+			$erro = \json_encode($erro);
+			throw new \Exception($erro);
+		}
+
+		$brl = new BrlConvert;
+		$total = $brl->convToUsd($amount['for'], $amount['to']);
+		$msg = [
+			'valorConvertido' 	=> $total,
+			'simbolo' 					=> $brl->simbol
+		];
+		$this->status(200);
+		echo \json_encode($msg);
+		die;
+	}
+
 }
