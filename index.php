aaa2b4d9b3f368d760875bbd2497e07e7ca019b9
alfredo <yoelvisjimenezr@gmail.com>
2021-10-31 00:57:37 -0500

    desafio completado

--- index.php
+++ index.php
@@ -8,10 +8,23 @@
  *
  * @category Challenge
  * @package  Back-end
- * @author   Seu Nome <seu-email@seu-provedor.com>
+ * @author   Yoelvis Alfredo Jimenez <@alfredojry>
  * @license  http://opensource.org/licenses/MIT MIT
  * @link     https://github.com/apiki/back-end-challenge
  */
 declare(strict_types=1);
 
 require __DIR__ . '/vendor/autoload.php';
+
+include __DIR__ . '/src/exchange.php';
+
+$endpoint = 'exchange';
+$params = array_filter(explode('/', $_SERVER['REQUEST_URI']));
+
+// valida único endpoint acetado
+if ($params[1] == $endpoint) {
+    $controllerExchange = new Exchange();
+    $controllerExchange->processRequest();
+} else {
+    header("HTTP/1.1 404 Not Found");
+}
