8e40bef5b8c922f31a33522bb08d4e9198a88e94
Bruno Andrade <brunoandrade1209@outlook.com>
2022-01-21 19:51:45 -0300

    InitialCommit

--- index.php
+++ index.php
@@ -15,3 +15,25 @@
 declare(strict_types=1);
 
 require __DIR__ . '/vendor/autoload.php';
+
+header('Content-Type: application/json');
+  
+if ($_SERVER["REQUEST_URI"]) {
+  $url = explode('/', $_SERVER['REQUEST_URI']);
+  array_shift($url);            
+  
+  if ($url[0] === 'exchange') {
+    $service = 'App\Services\\'.ucfirst($url[0]).'Service';
+    
+    array_shift($url);
+    
+    $method = strtolower($_SERVER['REQUEST_METHOD']);           
+    
+    $response = call_user_func_array([new $service, $method], $url);
+
+    http_response_code(200);
+    echo json_encode($response);
+    exit;      
+  }
+}
+    
