afda67a306d3ceb9afbdeb305d5ff18261a15ee9
Henrique Venchiarutti <henrique.venchiarutti@webjump.com.br>
2022-06-27 16:10:52 -0300

    Finalizando Teste API

--- index.php
+++ index.php
@@ -8,11 +8,32 @@
  *
  * @category Challenge
  * @package  Back-end
- * @author   Seu Nome <seu-email@seu-provedor.com>
+ * @author   Henrique Venchiarutti <henriquevenchiarutti@gmail.com>
  * @license  http://opensource.org/licenses/MIT MIT
- * @link     https://github.com/apiki/back-end-challenge
+ * @link     https://github.com/henriquevenchiarutti/back-end-challenge
  */
 declare(strict_types=1);
 
+use Challenge\Source\Model\Request;
+
+require_once __DIR__ . '/src/Model/Request.php';
+
 require __DIR__ . '/vendor/autoload.php';
+
+$request = new Request();
+$result = $request->handleRequest();
+
+if ($result) {
+    header('Content-Type: application/json');
+    header('HTTP/1.1 200');
+    $response['body'] = json_encode($result,  JSON_UNESCAPED_UNICODE);
+    echo $response['body'];
+} else {
+    header('Content-Type: application/json');
+    header('HTTP/1.1 400');
+    $response['body'] = json_encode([
+        'error' => "Invalid Parameters"
+    ], JSON_UNESCAPED_UNICODE);
+    echo $response['body'];
+}
 
