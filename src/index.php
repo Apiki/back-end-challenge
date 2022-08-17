235c49de0a51df1b3ce889bc1a9becab0baa360e
PX <41513193+EngineerDesigner@users.noreply.github.com>
2022-08-17 18:37:18 -0300

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,18 +1,31 @@
 <?php
 $paths = explode('/', $_SERVER['PATH_INFO']);
-$symb=["$","€","R$"];
-$setsymb;
 
-$paths[4] == "BRL" ? $setsymb = $symb[2] :"";
-$paths[4] == "EUR" ? $setsymb = $symb[1] :"";
-$paths[4] == "USD" ? $setsymb = $symb[0] :"";
+function exchange($path) {
 
-$resp = (object) array(
-    "valorConvertido"=>$paths[2] * $paths[5],
-    "simboloMoeda"=> $setsymb
-);
+    $symb=["$","€","R$"];
+    $setsymb;
+    $path[4] == "BRL" ? $setsymb = $symb[2] :"";
+    $path[4] == "EUR" ? $setsymb = $symb[1] :"";
+    $path[4] == "USD" ? $setsymb = $symb[0] :"";
+    
+    
+    $resp = (object) array(
+        "valorConvertido"=>$path[2] * $path[5],
+        "simboloMoeda"=> $setsymb
+    );
+    
+    echo '<script>console.log('.json_encode($resp).');</script>';
+    echo json_encode($resp);
+    
+    return json_encode($resp);
+    
+}
 
-echo '<script>console.log('.json_encode($resp).');</script>';
-echo json_encode($resp);
+if ($paths[1] == "exchange") {
+ 
+exchange($paths);
+   
+}
 
 ?>
