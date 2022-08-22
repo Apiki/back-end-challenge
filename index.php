96f42e21fe04c86073e6aecf4cabdbfaeee6bfdb
bernardomaia <bernardomgo@gmail.com>
2022-08-21 23:20:01 -0300

    desafio resolvido

--- index.php
+++ index.php
@@ -1,18 +1,46 @@
 <?php
+
 /**
  * Back-end Challenge.
  *
  * PHP version 7.4
  *
- * Este será o arquivo chamado na execução dos testes automátizados.
+ * Este será o arquivo chamado na execução dos testes automatizados.
  *
  * @category Challenge
  * @package  Back-end
- * @author   Seu Nome <seu-email@seu-provedor.com>
+ * @author   Bernardo Gomes <bernardomgo@gmail.com>
  * @license  http://opensource.org/licenses/MIT MIT
  * @link     https://github.com/apiki/back-end-challenge
  */
+
 declare(strict_types=1);
 
 require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/src/controller.php';
 
+$response = [];
+$request = array_filter(explode('/', $_SERVER['REQUEST_URI']));
+$method = array_shift($request);
+
+
+if (isset($method)) {
+    $c = new controller;
+    $response = $c->procRequest('_' . $method, $request);
+} else {
+    $response['ERRO'] = 'MÉTODO NÃO ENCONTRADO';
+}
+
+send_response($response);
+//
+function send_response($response)
+{
+    header("Content-Type:application/json");
+    if (!$response) { 
+        $response['ERRO'] = 'OPERAÇÃO NÃO RECONHECIDA';
+    }
+    if (isset($response['ERRO'])) {
+        http_response_code(400);
+    }
+    echo json_encode($response);
+}
