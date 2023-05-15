3d16c83f1bcc38a344751302156292d1c4c97801
Clayton Santos Cordeiro <claytonsantos13@hotmail.com>
2023-05-15 10:49:33 -0300

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,6 +1,23 @@
 
 <?php
 
+/**
+ * Back-end Challenge.
+ *
+ * PHP version 7.4
+ *
+ * Este será o arquivo chamado na execução dos testes automátizados.
+ *
+ * @category Challenge
+ * @package  Back-end
+ * @author   Clayton Santos Cordeiro <claytonsantos13@hotmail.com>
+ * @license  http://opensource.org/licenses/MIT MIT
+ * @link     https://github.com/apiki/back-end-challenge
+ */
+declare(strict_types=1);
+
+require __DIR__ . '/../vendor/autoload.php';
+
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Currency extends CI_Controller {
