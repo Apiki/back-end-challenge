fff493c1f3aeb929861d9a694f07166b8a26eb9b
Itamar Silva <itamarsilvacc@gmail.com>
2023-07-28 22:36:20 -0300

    Definindo constantes para moedas

--- src/CurrencyConverter.php
+++ src/CurrencyConverter.php
@@ -4,10 +4,19 @@ namespace App;
 
 class CurrencyConverter
 {
+    const CURRENCY_BRL = 'BRL';
+    const CURRENCY_USD = 'USD';
+    const CURRENCY_EUR = 'EUR';
+
     public function convert(float $amount, string $fromCurrency, string $toCurrency, float $rate): float
     {
         if ($amount <= 0 || $rate <= 0) {
             throw new \InvalidArgumentException("Invalid amount or rate.");
+        }
+
+        if (!in_array($fromCurrency, [self::CURRENCY_BRL, self::CURRENCY_USD, self::CURRENCY_EUR]) ||
+            !in_array($toCurrency, [self::CURRENCY_BRL, self::CURRENCY_USD, self::CURRENCY_EUR])) {
+            throw new \InvalidArgumentException("Invalid currency.");
         }
 
         $convertedAmount = $amount * $rate;
@@ -15,3 +24,4 @@ class CurrencyConverter
         return round($convertedAmount, 2);
     }
 }
+
